216cb0c0cc78a15256b7b6c8620a888f89e3fed7
Ewan Valentine <ewan.valentine89@gmail.com>
2017-12-25 17:11:54 +0000

    UPDATED to simplify the cli service

--- cli.go
+++ cli.go
@@ -5,66 +5,59 @@ import (
 	"os"
 
 	pb "github.com/EwanValentine/shippy-user-service/proto/user"
-	"github.com/micro/cli"
-	"github.com/micro/go-micro"
+	micro "github.com/micro/go-micro"
 	microclient "github.com/micro/go-micro/client"
 	"golang.org/x/net/context"
 )
 
 func main() {
 
-	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
+	srv := micro.NewService(
 
-	service := micro.NewService()
-
-	service.Init(
-
-		micro.Action(func(c *cli.Context) {
-
-			name := "Ewan Valentine"
-			email := "ewan.valentine89@gmail.com"
-			password := "test123"
-			company := "BBC"
-
-			log.Println(name, email, password)
-
-			r, err := client.Create(context.TODO(), &pb.User{
-				Name:     name,
-				Email:    email,
-				Password: password,
-				Company:  company,
-			})
-			if err != nil {
-				log.Fatalf("Could not create: %v", err)
-			}
-			log.Printf("Created: %s", r.User.Id)
-
-			getAll, err := client.GetAll(context.Background(), &pb.Request{})
-			if err != nil {
-				log.Fatalf("Could not list users: %v", err)
-			}
-			for _, v := range getAll.Users {
-				log.Println(v)
-			}
-
-			authResponse, err := client.Auth(context.TODO(), &pb.User{
-				Email:    email,
-				Password: password,
-			})
-
-			if err != nil {
-				log.Fatalf("Could not authenticate user: %s error: %v\n", email, err)
-			}
-
-			log.Printf("Your access token is: %s \n", authResponse.Token)
-
-			// let's just exit because
-			os.Exit(0)
-		}),
+		micro.Name("go.micro.srv.user-cli"),
+		micro.Version("latest"),
 	)
 
-	// Run the server
-	if err := service.Run(); err != nil {
-		log.Println(err)
+	// Init will parse the command line flags.
+	srv.Init()
+
+	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
+
+	name := "Ewan Valentine"
+	email := "ewan.valentine89@gmail.com"
+	password := "test123"
+	company := "BBC"
+
+	r, err := client.Create(context.TODO(), &pb.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Company:  company,
+	})
+	if err != nil {
+		log.Fatalf("Could not create: %v", err)
 	}
+	log.Printf("Created: %s", r.User.Id)
+
+	getAll, err := client.GetAll(context.Background(), &pb.Request{})
+	if err != nil {
+		log.Fatalf("Could not list users: %v", err)
+	}
+	for _, v := range getAll.Users {
+		log.Println(v)
+	}
+
+	authResponse, err := client.Auth(context.TODO(), &pb.User{
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not authenticate user: %s error: %v\n", email, err)
+	}
+
+	log.Printf("Your access token is: %s \n", authResponse.Token)
+
+	// let's just exit because
+	os.Exit(0)
 }
